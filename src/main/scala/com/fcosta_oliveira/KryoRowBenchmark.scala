4f7ab2bfca16cafd8a16052f8f3f2b5c7aa22116
fcosta_oliveira <filipecosta90@gmail.com>
2019-06-26 10:52:19 +0100

    [add] testDefaultSerializerSingleOutput with Row working

--- src/main/scala/com/fcosta_oliveira/KryoRowBenchmark.scala
+++ src/main/scala/com/fcosta_oliveira/KryoRowBenchmark.scala
@@ -6,6 +6,9 @@ import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.Output
 import org.apache.commons.lang3.StringUtils
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.expressions.{GenericRow, GenericRowWithSchema}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.objenesis.strategy.StdInstantiatorStrategy
 import org.openjdk.jmh.Main
 import org.openjdk.jmh.annotations._
 import org.slf4j.{Logger, LoggerFactory}
@@ -13,13 +16,13 @@ import org.slf4j.{Logger, LoggerFactory}
 @State(Scope.Benchmark)
 @BenchmarkMode(Array(Mode.AverageTime, Mode.Throughput))
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Measurement( time = 60 )
+@Measurement(time = 60)
 class KryoRowBenchmark {
 
   val kryo = new Kryo
   val records: Int = 100000
+  val seq: Array[Row] = new Array[Row](records)
   private val LOG: Logger = LoggerFactory.getLogger(classOf[Main])
-  var seq: Seq[Row] = Seq(Row())
   // colsize
   @Param(Array("36"))
   var colsize: Int = _
@@ -37,28 +40,35 @@ class KryoRowBenchmark {
   // Output for writing to a byte array.
   private var output = null
 
-
   // The method is called once for each time for each full run of the benchmark.
   // A full run means a full "fork" including all warmup and benchmark iterations.
   @Setup(Level.Trial)
   def setup(): Unit = {
-    kryo.register(classOf[org.apache.spark.sql.Row])
-    kryo.addDefaultSerializer(classOf[org.apache.spark.sql.Row], classOf[RowSerializer])
-    var recordsPos = 0;
-    val colPos = 0
+    kryo.addDefaultSerializer(classOf[Row], classOf[RowSerializer])
+    kryo.register(classOf[Row])
+    kryo.register(classOf[GenericRow])
+
+    kryo.getInstantiatorStrategy.asInstanceOf[Kryo.DefaultInstantiatorStrategy].setFallbackInstantiatorStrategy(new StdInstantiatorStrategy)
+
+
     data = StringUtils.repeat("x", colsize)
     LOG.info("started generating sequence data")
-    for (recordsPos <- 1 to records) {
-      var row : Row = Row()
-      for (colPos <- 1 to ncols) {
-        row.+(data)
+    for (recordsPos <- 0 to records - 1) {
+      val cols = new Array[Any](ncols)
+      val structfields = new Array[StructField](ncols)
+      for (colPos <- 0 to ncols - 1) {
+        cols(colPos) = data
+        structfields(colPos) = StructField("col" + colPos, StringType)
       }
-      seq ++= Seq(row)
+      var schema = StructType(structfields)
+      val newRow: Row = new GenericRowWithSchema(cols, schema)
+
+      seq(recordsPos) = newRow
     }
     LOG.info("ended generating sequence data")
 
     if (buffersize < ncols * colsize * blockSize) {
-      LOG.warn("Setting buffersize to " + ( ncols * colsize * blockSize * 2 ) + " since " + buffersize + " was too small ")
+      LOG.warn("Setting buffersize to " + (ncols * colsize * blockSize * 2) + " since " + buffersize + " was too small ")
       buffersize = ncols * colsize * blockSize * 2
     }
 
@@ -69,16 +79,16 @@ class KryoRowBenchmark {
   @OperationsPerInvocation(100000)
   def testDefaultSerializerSingleOutput(): Unit = {
     val output = new Output(buffersize)
-    var blockcount : Int = 0
-    seq.grouped(blockSize).foreach { row =>
+    var blockcount: Int = 0
+    seq.grouped(blockSize).foreach(arrayRow => {
       output.setPosition(0)
-      // convert Seq to Array since we need a Serializable
-
-      kryo.writeObject(output, row)
-      //Flushes any buffered bytes and closes the underlying OutputStream, if any.
+      arrayRow.foreach(row => {
+        kryo.writeObject(output, row)
+      })
       output.close()
-      blockcount+=1;
+      blockcount += 1;
     }
+    )
   }
 
 }
--- src/main/scala/com/fcosta_oliveira/RowSerializer.scala
+++ src/main/scala/com/fcosta_oliveira/RowSerializer.scala
@@ -3,35 +3,32 @@ package com.fcosta_oliveira
 import com.esotericsoftware.kryo.io.{Input, Output}
 import com.esotericsoftware.kryo.{Kryo, Serializer}
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{BooleanType, ByteType, DateType, DecimalType, DoubleType, FloatType, IntegerType, LongType, ShortType, StringType, TimestampType}
+import org.apache.spark.sql.types._
+import org.openjdk.jmh.Main
+import org.slf4j.{Logger, LoggerFactory}
 
+class RowSerializer extends Serializer[Row] {
+  private val LOG: Logger = LoggerFactory.getLogger(classOf[Main])
 
-class RowSerializer extends Serializer[org.apache.spark.sql.Row] {
-
-  override def write(kryo: Kryo, output: Output, t: org.apache.spark.sql.Row): Unit = {
-    t.schema.foreach(field => {
-      output.writeString(field.name)
-      field.dataType match {
-        case BooleanType => output.writeBoolean(t.getAs[Boolean](field.name))
-        case ByteType => output.writeByte(t.getAs[Byte](field.name))
-        case ShortType =>output.writeShort(t.getAs[Short](field.name))
-        case IntegerType => output.writeInt(t.getAs[Int](field.name))
-        case LongType =>output.writeLong(t.getAs[Long](field.name))
-        case FloatType => output.writeFloat(t.getAs[Float](field.name))
-        case DoubleType => output.writeDouble(t.getAs[Double](field.name))
-        case StringType => output.writeString(t.getAs[String](field.name))
-        //case _ => kryo.writeClassAndObject(output, t.getAs(field.name))
+  override def write(kryo: Kryo, output: Output, t: Row): Unit = {
+    if (t.size > 0) {
+      t.schema.foreach { field: StructField => {
+        output.writeString(field.name)
+        field.dataType match {
+          case BooleanType => output.writeBoolean(t.getAs[Boolean](field.name))
+          case ByteType => output.writeByte(t.getAs[Byte](field.name))
+          case ShortType => output.writeShort(t.getAs[Short](field.name))
+          case IntegerType => output.writeInt(t.getAs[Int](field.name))
+          case LongType => output.writeLong(t.getAs[Long](field.name))
+          case FloatType => output.writeFloat(t.getAs[Float](field.name))
+          case DoubleType => output.writeDouble(t.getAs[Double](field.name))
+          case StringType => output.writeString(t.getAs[String](field.name))
+          //case _ => kryo.writeClassAndObject(output, t.getAs(field.name))
+        }
       }
-    })
+      }
+    }
   }
 
-  override def read(kryo: Kryo, input: Input, aClass: Class[_ <: Row]): Row = {
-    ???
-  }
-
-//  override def read(kryo: Kryo, input: Input, aClass: Class[org.apache.spark.sql.Row]): Row = {
-//    //TODO: implement this
-//    val rowInstance = Row.fromSeq("TODO")
-//    rowInstance
-//  }
+  override def read(kryo: Kryo, input: Input, aClass: Class[Row]): Row = ???
 }
